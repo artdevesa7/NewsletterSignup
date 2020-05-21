322d5497dd9b74c535216ed0f5a65a26b2eb12a9
artdevesa7 <63007186+artdevesa7@users.noreply.github.com>
2020-05-21 17:03:59 -0400

    First Commit
    Uploading all files required to run the app.

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express()
+
+app.use(express.static("public"));
+//provide path, "public", of our static files since our app is not a static website anymore
+//and express needs to know where these static files are. refer to these static files by a relative
+//url. 
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/signup.html")
+});
+
+app.post("/", function(req,res){
+    const firstName = req.body.fName;
+    const secondName = req.body.sName;
+    const email = req.body.email;
+    
+    //console.log(firstName,secondName,email);
+    const data = {
+        members: [
+            {
+                email_address: email, 
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: secondName,
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us18.api.mailchimp.com/3.0/lists/4d743fe914";
+    //replace usX with the -us18 in api key
+
+    const options = {
+        method: "POST",
+        auth: "arturo:c58262b74d2fbb47af8dfb01782814a8-us18",
+    };
+
+    const request = https.request(url, options, function(response){
+        
+        if (response.statusCode == 200){
+            res.sendFile(__dirname+"/success.html");
+        } else {
+            res.sendFile(__dirname+"/failure.html");
+        }
+        
+        response.on("data", function(data){
+        console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure",function(req, res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function(){
+    //add a procfile with: web node app.js for heroku to know which file to start running
+    console.log("Server is running on port 3000")
+});
+
+// API Key 
+// c58262b74d2fbb47af8dfb01782814a8-us18
+
+//List ID
+//4d743fe914
